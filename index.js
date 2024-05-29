da8bf7e86b36cc4495cb149864f410aa1071215f
Muhammad Fahreza <147236874+ejatapibeda@users.noreply.github.com>
2024-05-29 20:17:46 +0700

    revert changes

--- index.js
+++ index.js
@@ -1,90 +1,79 @@
 const axios = require("axios");
 const { addonBuilder, serveHTTP } = require("stremio-addon-sdk");
-const { URL } = require("url");
 
-// Fungsi splitM3UFromUrl
-async function splitM3UFromUrl(m3uUrl) {
-  try {
-    const response = await axios.get(m3uUrl);
-
-    if (response.status !== 200) {
-      throw new Error("Failed to download M3U file");
-    }
-
-    const m3uContent = response.data;
-    const baseUrl = new URL(m3uUrl);
-
-    const resolutions = {
-      "640x360": null,
-      "854x480": null,
-      "1280x720": null,
-      "1920x1080": null,
-      auto: m3uUrl, // Default to the base m3u URL for auto
-    };
-
-    const lines = m3uContent.split("\n");
-    let currentResolution = null;
-
-    for (const line of lines) {
-      if (line.startsWith("#EXT-X-STREAM-INF:")) {
-        if (line.includes("RESOLUTION=")) {
-          currentResolution = line.split("RESOLUTION=")[1].split(",")[0];
-        } else {
-          currentResolution = "auto";
-        }
-      } else if (currentResolution) {
-        resolutions[currentResolution] = new URL(line, baseUrl.href).href;
-        currentResolution = null;
-      }
-    }
-
-    return resolutions;
-  } catch (error) {
-    throw new Error(`Error processing M3U file: ${error.message}`);
+async function getTmdbIdFromImdbId(imdbId) {
+  const response = await axios.get(
+    `https://api.themoviedb.org/3/find/${imdbId}?api_key=7ac6de5ca5060c7504e05da7b218a30c&external_source=imdb_id`
+  );
+  if (response.data.tv_results && response.data.tv_results.length > 0) {
+    return response.data.tv_results[0].id;
+  } else {
+    throw new Error("No TV results found for the given IMDb ID");
   }
 }
 
-class StreamHandler {
-  static async getTmdbIdFromImdbId(imdbId) {
-    const response = await axios.get(
-      `https://api.themoviedb.org/3/find/${imdbId}?api_key=7ac6de5ca5060c7504e05da7b218a30c&external_source=imdb_id`
-    );
-    if (response.data.tv_results && response.data.tv_results.length > 0) {
-      return response.data.tv_results[0].id;
-    } else {
-      throw new Error("No TV results found for the given IMDb ID");
-    }
+async function getStreamsFlixquest(url) {
+  try {
+    const response = await axios.get(url);
+    const { sources } = response.data;
+    return sources.map((source) => ({
+      url: source.url,
+      title: `🎞️ VidSrcTo - ${source.quality}`,
+    }));
+  } catch (error) {
+    throw new Error("Video not found");
   }
+}
 
-  static async getStreamsNewLink(url) {
-    try {
-      const response = await axios.get(url);
-      const data = response.data;
-      let streams = [];
+async function getStreamsVsrcme(url) {
+  try {
+    const response = await axios.get(url);
+    const data = response.data;
+    let streams = [];
 
-      if (data.source && data.source.startsWith("https://")) {
-        const sources = await splitM3UFromUrl(data.source);
-
-        for (const resolution in sources) {
-          if (sources.hasOwnProperty(resolution) && sources[resolution]) {
-            streams.push({
-              url: sources[resolution],
-              title: `🎞️ VidSrcTo - ${resolution}`,
-            });
-          }
-        }
+    for (const item of data) {
+      if (
+        !item.data ||
+        !item.data.file ||
+        !item.data.file.startsWith("https://")
+      ) {
+        continue;
       }
 
-      return streams;
-    } catch (error) {
-      throw new Error("Video not found");
+      streams.push({
+        url: item.data.file,
+        title: `🎞️ ${item.name} - Auto`,
+      });
     }
+
+    return streams;
+  } catch (error) {
+    throw new Error("Video not found");
+  }
+}
+
+async function getStreamsNewLink(url) {
+  try {
+    const response = await axios.get(url);
+    const data = response.data;
+    let streams = [];
+
+    if (data.source && data.source.startsWith("https://")) {
+      streams.push({
+        url: data.source,
+        title: `🎞️ VidSrcTo - Auto`,
+      });
+    }
+
+    return streams;
+  } catch (error) {
+    throw new Error("Video not found");
   }
 }
 
 const builder = new addonBuilder({
   id: "org.jamovies",
-  version: "1.2.2",
+  version: "1.2.1",
   name: "JaMovies",
   logo: "https://i.imgur.com/QhZlCx6.jpg",
   resources: ["stream"],
@@ -95,17 +84,51 @@ const builder = new addonBuilder({
 builder.defineStreamHandler(async ({ type, id }) => {
   let url;
   if (type === "movie") {
-    url = `https://vidsrc-api-bice.vercel.app/${id}`;
+    url = `https://flixquest-api.vercel.app/vidsrcto/watch-movie?tmdbId=${id}`;
   } else if (type === "series") {
     const [imdbId, season, episode] = id.split(":");
-    url = `https://vidsrc-api-bice.vercel.app/${imdbId}?s=${season}&e=${episode}`;
+    const tmdbId = await getTmdbIdFromImdbId(imdbId);
+    url = `https://flixquest-api.vercel.app/vidsrcto/watch-tv?tmdbId=${tmdbId}&season=${season}&episode=${episode}`;
   }
 
   try {
-    return { streams: await StreamHandler.getStreamsNewLink(url) };
+    if (url.includes("flixquest")) {
+      return { streams: await getStreamsFlixquest(url) };
+    }
   } catch (error) {
-    throw new Error("Failed to retrieve streams");
+    if (type === "movie") {
+      url = `https://vidsrc-api-bice.vercel.app/${id}`;
+    } else if (type === "series") {
+      const [imdbId, season, episode] = id.split(":");
+      url = `https://vidsrc-api-bice.vercel.app/${imdbId}?s=${season}&e=${episode}`;
+    }
+
+    try {
+      return { streams: await getStreamsNewLink(url) };
+    } catch (error) {
+      if (type === "movie") {
+        url = `https://srcvid.vercel.app/vsrcme/${id}`;
+      } else if (type === "series") {
+        const [imdbId, season, episode] = id.split(":");
+        const tmdbId = await getTmdbIdFromImdbId(imdbId);
+        url = `https://srcvid.vercel.app/${tmdbId}?s=${season}&e=${episode}`;
+      }
+
+      try {
+        return { streams: await getStreamsVsrcme(url) };
+      } catch (error) {
+        return Promise.reject();
+      }
+    }
   }
+});
+
+process.on("uncaughtException", function (err) {
+  console.error("Caught exception: ", err);
+});
+
+process.on("unhandledRejection", (reason, promise) => {
+  console.error("Unhandled Rejection at:", promise, "reason:", reason);
 });
 
 const addonInterface = builder.getInterface();
